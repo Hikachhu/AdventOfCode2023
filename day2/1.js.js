176f32c8014cb905f7d9105db507e8012edcdc77
Florian <florian.morin@gmail.com>
2023-12-03 14:42:25 +0100

    add 2A

--- day2/1.js.js
+++ day2/1.js.js
@@ -1,2 +1,14 @@
-
-
+const red = /[0-9]+ red/g;
+const blue = /[0-9]+ blue/g;
+const green = /[0-9]+ green/g;
+var message=document.querySelector("body > pre").textContent.split("\n").slice(0, -1)
+var decoupe=message.map(c=>{
+    var content=c.split(":")[1].split(";")
+    console.log(content)
+    var nbrRouge=content.map(d=>d.match(red)).filter(e=>e!=null).map(f=>Number(f[0].split(" ")[0])).reduce((a, b) => Math.max(a, b));
+    var nbrBlue=content.map(d=>d.match(blue)).filter(e=>e!=null).map(f=>Number(f[0].split(" ")[0])).reduce((a, b) => Math.max(a, b));
+    var nbrGreen=content.map(d=>d.match(green)).filter(e=>e!=null).map(f=>Number(f[0].split(" ")[0])).reduce((a, b) => Math.max(a, b));
+    if(nbrRouge>12 || nbrGreen>13||nbrBlue>14)return 0;
+    return Number(c.split(":")[0].split(" ")[1]);
+}).reduce((x, y) => {return x + y}, 0);
+console.log(decoupe)
